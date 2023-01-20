d35aa999dd297f9680d2d44577aee8da29b55000
anikdev2016 <anikdev2016@gmail.com>
2023-01-20 10:35:51 +0600

    basic mutation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
+// dummy data
+const POSTS = [
+  { id: 1, name: "Anik" },
+  { id: 2, name: "Partho" },
+];
 function App() {
   // we can do two things with react query.
   // ? 1. query --> getting the data from somewhere
   // ? 2. mutation --> posting the data to somewhere
-
-  // dummy data
-  const POSTS = [
-    { id: 1, name: "Anik" },
-    { id: 2, name: "Partho" },
-  ];
-
+  // to update the data instantly
+  const queryClient = useQueryClient();
+  console.log(POSTS);
   // querying data
   const postsQuery = useQuery({
     // inside the object first one will be a unique identifier
@@ -19,15 +20,38 @@ function App() {
     queryFn: () => wait(1000).then(() => [...POSTS]),
   });
 
+  // mutation (adding data)
+  const newPostMutation = useMutation({
+    // use mutation will also take object and inside of it we have to pass mutation function which will take promise as its value. we can pass functions there.
+    // and we always have to pass one single value in the promise
+    mutationFn: (name) => {
+      return wait(1000).then(() =>
+        POSTS.push({ id: Math.random().toString(), name })
+      );
+    },
+    // to update data instantly we have to invalidate this with the same key
+    onSuccess: () => {
+      queryClient.invalidateQueries("posts");
+    },
+  });
   // setting up loading
   if (postsQuery.isLoading) return <h1>Loading...</h1>;
   if (postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+
   return (
     <div>
       <h1>React query</h1>
       {postsQuery.data.map((post) => (
         <div key={post.id}>{post.name}</div>
       ))}
+
+      {/* to add something we have to call mutate function inside newPostMutation function and pass the parameter there (promised one) */}
+      <button
+        disabled={newPostMutation.isLoading}
+        onClick={() => newPostMutation.mutate("Antu")}
+      >
+        Add new
+      </button>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 // import query client and client provider
-import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
